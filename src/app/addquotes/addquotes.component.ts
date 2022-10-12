97084e2116863149445b7e344e12b1e320c31f48
aravindsabu <aravindsabu2001@gmail.com>
2022-10-12 12:21:58 +0530

    ok

---
+++ src/app/addquotes/addquotes.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addquotes',
+  templateUrl: './addquotes.component.html',
+  styleUrls: ['./addquotes.component.css']
+})
+export class AddquotesComponent implements OnInit {
+
+  constructor() { }
+  add={}
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AddquotesComponent } from './addquotes/addquotes.component';
+import { RouterModule, Routes } from '@angular/router';
+const appRoutes:Routes=[
+  {
+    path:"",component:AddquotesComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AddquotesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
